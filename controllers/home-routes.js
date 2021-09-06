7c2acc4b56e12ea2002cc08cd2cfba945256cdf7
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-06 12:05:49 -0700

    serialize added to viewpost in home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,6 +36,49 @@ const { User, Post, Comment } = require("../models");
         });
  });
 
+ router.get("/viewpost/:id", (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "body", "user_id"],
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          as: "comments",
+          attributes: ["id", "comment_text", "user_id"],
+          include: [
+            {
+              model: User,
+              as: "user",
+              attributes: ["username"],
+            },
+          ],
+        },
+      ],
+    })
+      .then((dbPostData) => {
+        //serialize data
+        if (!dbPostData) {
+          res.status(404).json({ message: "No Posts Available" });
+          return;
+        }
+        const post = dbPostData.get({ plain: true }); // serialize posts
+        console.log(post);
+        res.render("single-post", { post });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+ 
+
  //serve up login page
  router.get("/login", (req, res) => {
     res.render("login");
