4129e901ad2e6b2b55032de7d80ad98cbe642e7e
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-06 11:47:49 -0700

    routes for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,18 +1,60 @@
 const router = require("express").Router();
 
+const { User, Post, Comment } = require("../../models");
+
  //get all the users
  router.get("/", (req, res) => {
-   res.send("GET ALL users");
+    User.findAll({
+        attributes: ["id", "username", "email", "password"],
+      }) 
+        .then((dbUserData) => {
+          res.json(dbUserData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
  });
 
  //get user by id
  router.get("/:id", (req, res) => {
-   res.send(`GET user WHERE id is ${req.params.id}`);
+    User.findOne(
+        {
+          where: {
+            id: req.params.id,
+          },
+        },
+        {
+          attributes: ["id", "username", "email", "password"], 
+        }
+      ) 
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            res.status(404).json({ message: "No User found with this id" });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
  });
 
  //add user
  router.post("/", (req, res) => {
-   res.send(`ADD user`);
+    User.create({
+        //expects username, email, password
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      })
+        .then((dbUserData) => {
+          res.json(dbUserData);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
  });
 
  //update user
@@ -20,7 +62,22 @@ const router = require("express").Router();
    res.send(`update user`);
  });
  //update user
- router.delete("/", (req, res) => {
-   res.send(`delete user`);
+ router.delete("/:id", (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
  });
  module.exports = router;
